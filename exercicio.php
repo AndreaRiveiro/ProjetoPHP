92f12be96f927b4ca9eba20d0d00a936853471a7
unknown <Digital@DH-A10-21.dhbrasil.digitalhouse.com>
2019-04-24 12:14:22 -0300

    aula php2

---
+++ exercicio.php
@@ -0,0 +1,18 @@
+$num1= 6;
+$num2= 8;
+
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>Page Title</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" type="text/css" media="screen" href="main.css" />
+    <script src="main.js"></script>
+
+</head>
+<body>
+    <? $n= $num1>$num2? $num1:$num2; ?>
+</body>
+</html>
